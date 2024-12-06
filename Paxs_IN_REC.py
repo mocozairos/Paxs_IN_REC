b2587c7209ea709e174c8ce58a596781763fbd78
mocozairos <mcruzdelirafilho@gmail.com>
2024-12-06 10:57:44 -0300

    Update Paxs_IN_REC.py

--- Paxs_IN_REC.py
+++ Paxs_IN_REC.py
@@ -2,7 +2,6 @@ import pandas as pd
 import mysql.connector
 import decimal
 import streamlit as st
-# Teste Commit
 
 def gerar_df_phoenix(vw_name):
     # Parametros de Login AWS
@@ -66,6 +65,8 @@ st.divider()
 
 if len(periodo)>1:
 
+    st.subheader('TRF IN')
+
     data_inicial = periodo[0]
 
     data_final = periodo[1]
@@ -74,15 +75,14 @@ if len(periodo)>1:
                                                     (st.session_state.mapa_router['Data Execucao'] <= data_final) & 
                                                     (st.session_state.mapa_router['Tipo de Servico']=='IN') & 
                                                     (st.session_state.mapa_router['Status do Servico']!='CANCELADO') & 
-                                                    (st.session_state.mapa_router['Status do Servico']!='RASCUNHO')].reset_index(drop=True)
+                                                    (st.session_state.mapa_router['Status do Servico']!='RASCUNHO') & 
+                                                    (st.session_state.mapa_router['Status da Reserva']!='CANCELADO')].reset_index(drop=True)
     
     df_mapa_filtrado['Paxs Totais'] = df_mapa_filtrado['Total ADT'] + df_mapa_filtrado['Total CHD']
 
-    adt_in = int(df_mapa_filtrado['Total ADT'].sum())
+    paxs_totais = int(df_mapa_filtrado['Paxs Totais'].sum())
 
-    chd_in = int(df_mapa_filtrado['Total CHD'].sum())
-
-    st.success(f'No período selecionado existem {adt_in + chd_in} passageiros.')
+    st.success(f'No período selecionado existem {paxs_totais} passageiros.')
 
     df_mapa_filtrado_group = df_mapa_filtrado.groupby('Servico')['Paxs Totais'].sum().reset_index()
 
@@ -97,3 +97,69 @@ if len(periodo)>1:
     with row1[1]:
 
         st.dataframe(df_mapa_filtrado_group_parceiro.sort_values(by='Paxs Totais', ascending=False), hide_index=True)
+
+    st.divider()
+
+    st.subheader('TRF OUT')
+
+    df_mapa_filtrado = st.session_state.mapa_router[(st.session_state.mapa_router['Data Execucao'] >= data_inicial) & 
+                                                    (st.session_state.mapa_router['Data Execucao'] <= data_final) & 
+                                                    (st.session_state.mapa_router['Tipo de Servico']=='OUT') & 
+                                                    (st.session_state.mapa_router['Status do Servico']!='CANCELADO') & 
+                                                    (st.session_state.mapa_router['Status do Servico']!='RASCUNHO') & 
+                                                    (st.session_state.mapa_router['Status da Reserva']!='CANCELADO')].reset_index(drop=True)
+    
+    df_mapa_filtrado['Paxs Totais'] = df_mapa_filtrado['Total ADT'] + df_mapa_filtrado['Total CHD']
+
+    paxs_totais = int(df_mapa_filtrado['Paxs Totais'].sum())
+
+    st.success(f'No período selecionado existem {paxs_totais} passageiros.')
+
+    df_mapa_filtrado_group = df_mapa_filtrado.groupby('Servico')['Paxs Totais'].sum().reset_index()
+
+    row2 = st.columns(2)
+
+    with row2[0]:
+
+        st.dataframe(df_mapa_filtrado_group.sort_values(by='Paxs Totais', ascending=False), hide_index=True)
+
+    df_mapa_filtrado_group_parceiro = df_mapa_filtrado.groupby('Parceiro')['Paxs Totais'].sum().reset_index()
+
+    with row2[1]:
+
+        st.dataframe(df_mapa_filtrado_group_parceiro.sort_values(by='Paxs Totais', ascending=False), hide_index=True)
+
+    st.divider()
+
+    st.subheader('Passeios')
+
+    df_mapa_filtrado = st.session_state.mapa_router[(st.session_state.mapa_router['Data Execucao'] >= data_inicial) & 
+                                                    (st.session_state.mapa_router['Data Execucao'] <= data_final) & 
+                                                    (st.session_state.mapa_router['Tipo de Servico'].isin(['TOUR', 'TRANSFER'])) & 
+                                                    (st.session_state.mapa_router['Status do Servico']!='CANCELADO') & 
+                                                    (st.session_state.mapa_router['Status do Servico']!='RASCUNHO') & 
+                                                    (st.session_state.mapa_router['Status da Reserva']!='CANCELADO')].reset_index(drop=True)
+    
+    df_mapa_filtrado['Paxs Totais'] = df_mapa_filtrado['Total ADT'] + df_mapa_filtrado['Total CHD']
+
+    paxs_totais = int(df_mapa_filtrado['Paxs Totais'].sum())
+
+    st.success(f'No período selecionado existem {paxs_totais} passageiros.')
+
+    df_mapa_filtrado_group = df_mapa_filtrado.groupby('Servico')['Paxs Totais'].sum().reset_index()
+
+    row2 = st.columns(2)
+
+    with row2[1]:
+
+        lista_servicos = sorted(df_mapa_filtrado_group['Servico'].unique().tolist())
+
+        filtro_servicos = st.multiselect('Filtrar Serviços', lista_servicos, default=None)
+
+    if filtro_servicos:
+
+        df_mapa_filtrado_group = df_mapa_filtrado_group[df_mapa_filtrado_group['Servico'].isin(filtro_servicos)].reset_index(drop=True)
+
+    with row2[0]:
+
+        st.dataframe(df_mapa_filtrado_group.sort_values(by='Paxs Totais', ascending=False), hide_index=True)
